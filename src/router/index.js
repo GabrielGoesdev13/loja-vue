d91913fe972290db26777bf531b3567037a83096
Gabriel <engs-gabrielgoes@camporeal.edu.br>
2026-06-09 19:52:47 -0300

    adicionado index,js

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import ProdutosView from '../views/ProdutosView.vue'
+import CarrinhoView from '../views/CarrinhoView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/produtos',
+      name: 'produtos',
+      component: ProdutosView
+    },
+    {
+      path: '/carrinho',
+      name: 'carrinho',
+      component: CarrinhoView
+    }
+  ]
 })
 
 export default router
